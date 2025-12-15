3c3d51257a45d3e8985363cf2a174568ae2f953c
Ghost <duchojakub987@gmail.com>
2025-12-15 13:38:30 +0100

    resizable window

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use crossterm::{ 
-    terminal::{Clear, ClearType, size, enable_raw_mode, disable_raw_mode},
+    terminal::{Clear, ClearType, enable_raw_mode, disable_raw_mode, size},
     queue,
-    cursor::{MoveTo},
     event::{self, KeyCode, Event},
+    cursor::{MoveTo},
 };
 use std::{
     io::{self, Write}, 
@@ -11,13 +11,13 @@ use std::{
 fn main() -> io::Result<()> {
     enable_raw_mode()?;
     let mut stdout = io::stdout();
-    let (w, h) = size()?;
     let label = b"Hello, World!";
-    let _ = queue!(stdout, Clear(ClearType::All));
-    let _ = queue!(stdout, MoveTo(w / 2 - label.len() as u16 / 2, h / 2));
-    stdout.write(label)?;
-    stdout.flush()?;
     loop{
+        let _ = queue!(stdout, Clear(ClearType::All));
+        let (w, h) = size()?;
+        let _ = queue!(stdout, MoveTo( w / 2 - label.len() as u16 / 2, h / 2));
+        stdout.write(label)?;
+        stdout.flush()?;
         if let Event::Key(key) = event::read()? {
             match key.code {
                 KeyCode::Char('q') => break,
