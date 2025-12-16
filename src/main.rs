1f04c6e618176b4b55df99b69f0787f9d4e142c7
Ghost <duchojakub987@gmail.com>
2025-12-16 13:04:13 +0100

    navigation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use crossterm::{ 
-    terminal::{Clear, ClearType, enable_raw_mode, disable_raw_mode},
+    terminal::{Clear, ClearType, enable_raw_mode, disable_raw_mode, size},
     queue,
     event::{self, KeyCode, Event},
-    cursor::{MoveTo},
+    cursor::{MoveTo, MoveUp, MoveDown},
 };
 use std::{
     io::{self, Write}, 
@@ -15,6 +15,28 @@ enum ElementType {
     File,
     Directory,
     HiddenFile,
+}
+
+enum Mode {
+    Navigating,
+}
+
+struct Global {
+    index: usize,
+    t_height: u16,
+    t_width: u16,
+    mode: Mode,
+}
+impl Global {
+    pub fn new() -> Self {
+        let (t_width, t_height) = size().unwrap();
+        Global { 
+            index: 0, 
+            t_height: t_height, 
+            t_width: t_width, 
+            mode: Mode::Navigating, 
+        }
+    }
 }
 
 struct Element {
@@ -59,20 +81,42 @@ fn get_elements_from_dir(path: &str, dir: &mut Dir) -> Result<(), Box<dyn Error>
 fn main() -> io::Result<()> {
     enable_raw_mode()?;
     let mut dir = Dir::new();
+    let mut global = Global::new();
     let _ = get_elements_from_dir(".", &mut dir);
     let mut stdout = io::stdout();
+    let mut shown = false;
     loop{
         let _ = queue!(stdout, Clear(ClearType::All));
         for (i, element) in dir.elements.iter().enumerate() {
             let _ = queue!(stdout, MoveTo(0, i as u16));
             let name = element.name.to_string_lossy();
-            let _ = stdout.write(name.as_bytes());
-            stdout.flush()?;
+            if i == global.index {
+                let selected_name = format!("{name} <-");
+                stdout.write(selected_name.as_bytes())?;
+            }else {
+                stdout.write(name.as_bytes())?;
+            }
         }
-
+        let _ = queue!(stdout, MoveTo(0, global.t_height - 2));
+        stdout.write(("─".repeat(global.t_width as usize)).as_bytes())?;
+        let _ = queue!(stdout, MoveTo(0, global.t_height));
+        stdout.flush()?;
         if let Event::Key(key) = event::read()? {
             match key.code {
                 KeyCode::Char('q') => break,
+                KeyCode::Char('j') => {
+                    if global.index < dir.elements.len() - 1 {
+                        global.index += 1;
+                    }
+                }
+                KeyCode::Char('k') => {
+                    if global.index > 0 {
+                        global.index -= 1;
+                    }
+                }
+                KeyCode::Enter => {
+                    todo!();
+                }
                 _ => {},
             }
         }
